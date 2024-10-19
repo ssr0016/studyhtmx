0f67b886d38c09084e1d64c9a0124ce82c954ea5
ssr0016 <samsonrecluta0016@gmail.com>
2024-10-19 23:11:53 +0800

    registration form

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/gorilla/mux v1.8.1
 	github.com/gorilla/sessions v1.4.0
 	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.28.0
 )
 
 require github.com/gorilla/securecookie v1.1.2 // indirect
--- main.go
+++ main.go
@@ -3,21 +3,27 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"html/template"
+	"htmx/pkg/handlers"
 	"log"
 	"net/http"
-	"text/template"
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
+
 	_ "github.com/lib/pq"
 )
 
 var tmpl *template.Template
 var db *sql.DB
-var Store = sessions.NewCookieStore([]byte("user-management-secret"))
+var Store = sessions.NewCookieStore([]byte("usermanagementsecret"))
 
-func init() {
-	tmpl, _ = template.ParseGlob("templates/*.html")
+func initTemplate() {
+	var err error
+	tmpl, err = template.ParseGlob("templates/*.html")
+	if err != nil {
+		log.Fatalf("Failed to parse templates: %v", err)
+	}
 
 	// Set up sessions
 	Store.Options = &sessions.Options{
@@ -32,11 +38,11 @@ func initDB() {
 	connStr := "postgres://postgres:secret@localhost:5432/profile?sslmode=disable"
 
 	// Open the connection
-	db, err := sql.Open("postgres", connStr)
+	var err error
+	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatalf("Failed to connect to PostgreSQL: %v", err)
 	}
-	defer db.Close()
 
 	// Check if the connection is valid
 	err = db.Ping()
@@ -48,8 +54,10 @@ func initDB() {
 }
 
 func main() {
-
 	gRouter := mux.NewRouter()
+
+	// Initialize templates
+	initTemplate()
 
 	// Initialize the database
 	initDB()
@@ -57,8 +65,15 @@ func main() {
 
 	// Routes
 	gRouter.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.ExecuteTemplate(w, "home.html", nil)
+		err := tmpl.ExecuteTemplate(w, "home.html", nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
+
+	gRouter.HandleFunc("/register", handlers.RegisterPage(db, tmpl)).Methods("GET")
+
+	gRouter.HandleFunc("/register", handlers.RegisterHandler(db, tmpl)).Methods("POST")
 
 	// Start server
 	http.ListenAndServe(":4000", gRouter)
